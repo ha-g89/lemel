0776ba0f72caac4e853c00b54608dc454d04db97
Mel Cayci <melcayci@gmail.com>
2026-09-24 20:24:35 +0200

    Voeg verhalen toe voor MCA_0585, MCA_0406, MCA_1123, MCA_1127, MCA_1181

--- src/data/verhalen.js
+++ src/data/verhalen.js
@@ -7,4 +7,14 @@
 export const VERHAAL = {
   MCA_1429:
     'Daikoku. De autodroom van elke liefhebber. Op dit specifieke plaatje kun je de legendarische Honda NSX bewonderen. Het model dat is getest door Ayrton Senna zelf. Dit exemplaar zul je niet veel zien in Nederland. Wat een schoonheid.',
+  MCA_0585:
+    'Dit beeldhouwwerk kwam ik tegen in een zijpaadje ergens in de buurt van het park rond Kanazawa kasteel. Na enig google werk vind ik daarover het volgende: De Hakuchouro-wandelroute staat ook wel bekend als het "pad van water, groen en bezinning". Langs deze route staan in totaal zo\'n twintig sculpturen van lokale kunstenaars, waaronder \'Een kleine wens\', waardoor de wandeling aanvoelt als een bezoek aan een openluchtmuseum.',
+  MCA_0406:
+    'Dit is een ginkgo blad. Ook wel bekend als de eendenpootboom, hoe leuk is dat? Deze is afkomstig van een ginkgoboom van ruim tweeduizend jaar oud die we zijn tegengekomen in een tempel in Takayama.',
+  MCA_1123:
+    'Bruuuuuuuuuuuuuuuuuuuuuuuuuuuuug. Ik hou van architectonische hoogstandjes. Zeker als het ook lekker contrasteert met de omgeving. In dit geval staan we op een eiland van een eiland in Okinawa. Het doet me ook denken aan beelden die ik gezien heb in Miami.',
+  MCA_1127:
+    'Japan heeft mij doen realiseren dat elk moment en plek een bijzondere compositie kan presenteren. Hier zie je een bijzonder rood autotje, op een parkeerplaats. Alsof iemand hem dat met de wetenschap heeft gedaan dat het gewoon goed voelt.',
+  MCA_1181:
+    'Een wilde Nissan GTR R-32, gespot in Ginza, Tokyo. Mijn persoonlijke favoriet. Keer op keer als ik deze Nissan zie, wil ik er een foto van maken. Puur om het feit dat je ze gewoon weinig ziet in Europa en in deze conditie waarin het lijkt alsof hij net uit de fabriek is gerold. De compositie zelf vond ik op dit specifieke plaatje ook leuk door de auto lager te plaatsen op de foto om zo het gevoel te geven alsof we ons in een grote stad bevinden (waar we natuurlijk ook waren) :).',
 }
